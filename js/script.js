ee8fa481a7ea4d81f1307331474ca37e12fed9d8
Code Flame <119540857+Higorxzzz@users.noreply.github.com>
2023-12-24 11:54:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
+/*==================== span text ====================*/
 ScrollReveal({
     reset: true,
     distance: '80px',
@@ -67,27 +68,49 @@ const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-toggle-right'
 
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
 
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*==================== form contact ====================*/
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('service_5a72ci8', 'template_fvskujf', '#contact-form', 'FUFqKIistmIQV39YA')
+    .then(
+      (response) => {
+        console.log('Resposta da API:', response);
+        contactMessage.textContent = 'Mensagem enviada com sucesso! ✅';
+
+        setTimeout(() =>{
+          contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+      },
+      (error) => {
+        console.error('Erro ao enviar e-mail:', error);
+        contactMessage.textContent = 'Mensagem não enviada (erro de serviço) ❌';
+      }
+    );
+};
+
+contactForm.addEventListener('submit', sendEmail);
